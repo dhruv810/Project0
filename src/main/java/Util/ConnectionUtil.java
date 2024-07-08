9929f315b8ff3b05514e939537d2c095b7805580
dhruv810 <dhruvfadadu11@gmail.com>
2024-07-08 13:15:36 -0700

    added support for docker postgres

--- src/main/java/Util/ConnectionUtil.java
+++ src/main/java/Util/ConnectionUtil.java
@@ -1,10 +1,47 @@
 package Util;
 
+import org.apache.ibatis.jdbc.ScriptRunner;
+import org.postgresql.ds.PGPoolingDataSource;
+import org.postgresql.jdbc2.optional.ConnectionPool;
+import org.postgresql.jdbc2.optional.SimpleDataSource;
+
+import javax.sql.DataSource;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionUtil {
 
-    private static String url = "";
-    private static String username = "";
-    private static String password = "";
 
+    private static final String url = "jdbc:postgresql://localhost:5432/project0";
+    private static final String user = "user";
+    private static final String password = "password";
+
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println("Sql exception: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void startDatabase() {
+        try {
+            FileReader sqlScript = new FileReader("src/main/resources/database.sql");
+            // run script
+            ScriptRunner sr = new ScriptRunner(getConnection());
+            sr.runScript(sqlScript);
+            System.out.println("Database created.");
+            sqlScript.close();
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
